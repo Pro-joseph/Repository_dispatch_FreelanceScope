1d1dd2999b6a1d06a5bec33121c2f1861fd733fb
Joseph <jdirayoussef1997@gmail.com>
2026-07-28 15:24:06 +0100

    Add S3 bucket creation and backend configuration for Terraform

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "freelancescope-terraform-state"
+    key    = "infra/terraform.tfstate"
+    region = "eu-west-3"
+  }
 }
 
 provider "aws" {
