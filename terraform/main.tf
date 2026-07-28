fe9a5c0479752293ffa859f4835923b9977c8e72
Joseph <jdirayoussef1997@gmail.com>
2026-07-28 15:17:40 +0100

    Update AMI filter to use the correct Ubuntu version

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,6 +69,6 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
 }
